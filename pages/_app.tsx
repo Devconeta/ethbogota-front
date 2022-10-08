ea22f5ef01c91b4d3553556e42cda52a91a02f45
nvcho <cu.ouza@gmail.com>
2022-10-08 02:18:46 -0300

    [UPDATE] dependencies

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,35 +1,8 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
-import "@rainbow-me/rainbowkit/styles.css";
-
-import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
-import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
-import { publicProvider } from "wagmi/providers/public";
-
-const { chains, provider } = configureChains(
-  [chain.polygon], [publicProvider()]
-);
-
-const { connectors } = getDefaultWallets({
-  appName: "My RainbowKit App",
-  chains,
-});
-
-const wagmiClient = createClient({
-  autoConnect: true,
-  connectors,
-  provider,
-});
-
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <WagmiConfig client={wagmiClient}>
-      <RainbowKitProvider chains={chains}>
-        <Component {...pageProps} />
-      </RainbowKitProvider>
-    </WagmiConfig>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
