355a988b0127ac18eede2adac0d2bff1a8c68a2b
nvcho <cu.ouza@gmail.com>
2022-10-08 17:48:54 -0300

    [ADD] connect

--- components/Main/Header.tsx
+++ components/Main/Header.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
 
 const Header = () => {
   const [walletAccount, setWalletAccount] = useState({
@@ -8,12 +9,29 @@ const Header = () => {
   });
   const [network, setNetwork] = useState("");
 
+  const browserProvider = new ethers.providers.Web3Provider(window.ethereum, "any");
+
+  useEffect(() => {
+    if (window.ethereum != null) {
+      getNetwork();
+
+      window.ethereum.on("chainChanged", () => {
+        // @dev - To prevent issues and critical bugs, the best practice is to reload the page when the user changes the network
+        window.location.reload();
+      });
+
+      return () => {
+        window.ethereum.removeAllListeners();
+      };
+    }
+  }, []);
+
   const connectWallet = async () => {
     if (window.ethereum) {
       try {
         const account = await window.ethereum.request({ method: "eth_requestAccounts" });
 
-        if (network == "gnosis" || network == "polygon") {
+        if (network == "xdai" || network == "matic") {
           setWalletAccount({ isConnected: true, address: account[0], wrongNetwork: false });
         } else {
           setWalletAccount({ isConnected: false, address: "", wrongNetwork: true });
@@ -27,13 +45,21 @@ const Header = () => {
   const getNetwork = async () => {
     const network = await browserProvider.getNetwork();
 
-    setNetwork({ name: network.name, chainId: network.chainId });
+    setNetwork(network.name);
   };
 
   return (
     <div className="flex flex-row justify-between px-10 pt-5">
       <h1>Web3 Storage</h1>
-      <button onClick={connectWallet}>Connect Wallet</button>
+
+      {walletAccount.isConnected ? (
+        <div className="flex flex-col items-center justify-center">
+          <p className="mr-2">{walletAccount.address}</p>
+          <button className="px-4 py-2 text-white bg-blue-500 rounded-md">Disconnect</button>
+        </div>
+      ) : (
+        <button onClick={connectWallet}>Connect Wallet</button>
+      )}
     </div>
   );
 };
